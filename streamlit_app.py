4c9fe99ad68dcbbb060346b9197d4fe4f62a3c18
Baazigar <rbadiger@yahoo.com>
2024-07-10 21:22:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ st.write("""Choose the fruits you want in your custom Smoothie!!""")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
-st.text(fruityvice_response.json())
 fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True)
 
 #option = st.selectbox(
