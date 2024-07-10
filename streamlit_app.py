506931db2cdf7a634d092ee8427e124c49682e32
Baazigar <rbadiger@yahoo.com>
2024-07-10 20:55:24 +0300

    Update streamlit_app.py
    copied the working code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,21 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
+import requests
 
 
 
 # Set the title of the app
-st.title('My Parents Smoothie Store!')
+st.title('Welcome to the Smoothie Store!')
 
 name_on_smoothie = st.text_input("Name on the Smoothie:")
 st.write("""Name on the Smoothie: """, name_on_smoothie)
 
 st.write("""Choose the fruits you want in your custom Smoothie!!""")
 
-
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response.json())
+#fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True)
 
 #option = st.selectbox(
 #    'What is your favorite fruit?',
@@ -22,7 +23,7 @@ st.write("""Choose the fruits you want in your custom Smoothie!!""")
 #
 
 #st.write('Your favorite fruit is:', option)
-session = get_active_session()
+#session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -38,32 +39,33 @@ if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
 
-    ingredients_string = ''
-    #name_on_order = 'Rafeeq'
+       ingredients_string = ''
+       #name_on_order = 'Rafeeq'
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+       for fruit_chosen in ingredients_list:
+           ingredients_string += fruit_chosen + ' '
+           st.subheader(fruit_chosen + ' Nutrition Information')
+           fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+           fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True)
 
-    #st.write(ingredients_string)
+           #st.write(ingredients_string)
 
 
-    my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order)
-                  values ('""" + ingredients_string + """','""" +name_on_smoothie+ """')"""
+       my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order)
+       values ('""" + ingredients_string + """','""" +name_on_smoothie+ """')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop()
-    #yes_emoji = "\u2714"
+       #st.write(my_insert_stmt)
+       #st.stop()
+       #yes_emoji = "\u2714"
 
-    # Using Unicode characters
-    yes_emoji = "\u2705"  # White Heavy Check Mark         
-    time_to_insert = st.button('Submit Your Order')
-
-    if time_to_insert:
-         yes_emoji = "\u2705"  # White Heavy Check Mark
-         session.sql(my_insert_stmt).collect()
-
-         st.write(f"Your Smoothie is ordered! {name_on_smoothie} {yes_emoji}")
-   
+       # Using Unicode characters
+       yes_emoji = "\u2705"  # White Heavy Check Mark         
+       time_to_insert = st.button('Submit Your Order')
+       yes_emoji = "\u2705"  # White Heavy Check Mark
+       if time_to_insert:
+               
+               session.sql(my_insert_stmt).collect()
+               st.write(f"Your Smoothie is ordered! {name_on_smoothie} {yes_emoji}")
 
 
 
